e48de37aa465c48f162baad665522fe7b946433b
Arkodeep Sen <59617363+arkodeepsen@users.noreply.github.com>
2022-12-02 23:01:57 +0530

    Create datatypes.js

---
+++ Data Types/datatypes.js
@@ -0,0 +1,13 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    
+    const firstDecimal = 4.0;
+   
+    const firstString = 'HackerRank ';
+    
+    console.log(parseInt(firstInteger)+parseInt(secondInteger));
+    
+    console.log(parseFloat(firstDecimal)+parseFloat(secondDecimal));
+    
+    console.log(firstString+secondString);
+}
