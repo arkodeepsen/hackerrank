e466eaa47cccb677e616d966b91ffdc670f93c64
Arkodeep Sen <arkothegamer72@gmail.com>
2022-12-04 21:20:16 +0530

    Create 2.java

---
+++ Java Stdin And Stdout/II/2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String s="amogus";
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        s=scan.nextLine();
+        s=scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
